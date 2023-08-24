c8a2ce5c38504953ba977cfc13afaa4feaf72c07
iaalm <iaalmsimon@gmail.com>
2023-08-24 15:12:51 +0800

    set parame -v for verbose log

--- wwwml/__main__.py
+++ wwwml/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 from os import environ
 
 import openai
@@ -10,6 +11,16 @@ INIT_PROMPT = "My Linux server is slow. Use providered function to check the ser
 
 
 def main():
+    parser = argparse.ArgumentParser(description="What's wrong with my linux?")
+    parser.add_argument(
+        "-p",
+        default=INIT_PROMPT,
+        type=str,
+        help="Initial prompt to start the conversation.",
+    )
+    parser.add_argument("-v", action="count", default=0, help="verbose mode")
+    args = parser.parse_args()
+
     if "WWWML_DEPLOYMENT" in environ:
         llm = ChatOpenAI(temperature=0, deployment_id=environ["WWWML_DEPLOYMENT"])
     else:
@@ -17,9 +28,9 @@ def main():
             temperature=0, model=environ.get("WWWML_MODEL", "gpt-3.5-turbo")
         )
     agent = initialize_agent(
-        load_tools(llm), llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True
+        load_tools(llm), llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=args.v > 0
     )
-    agent.run(INIT_PROMPT)
+    print(agent.run(args.p))
 
 
 if __name__ == "__main__":
