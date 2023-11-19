6c338d2734e7ce673e7afce8661e44bcffae9da7
iaalm <iaalmsimon@gmail.com>
2023-11-19 16:03:01 +0800

    use conversational retrieval agent

--- mmon/engine.py
+++ mmon/engine.py
@@ -1,5 +1,5 @@
 import openai
-from langchain.agents import AgentType, initialize_agent
+from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 from langchain.memory import ConversationBufferMemory
 
 from mmon.tools import load_tools
@@ -8,18 +8,15 @@ from mmon.tools import load_tools
 class Engine:
     def __init__(self, llm, verbose_level=0):
         tools = load_tools(llm)
-        memory = ConversationBufferMemory(
-            memory_key="chat_history", return_messages=True
-        )
         if verbose_level > 2:
             openai.log = "debug"
 
-        self.executor = initialize_agent(
-            tools,
-            llm,
-            agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+        self.executor = create_conversational_retrieval_agent(
+            llm=llm,
+            tools=tools,
+            max_token_limit=2000,
+            remember_intermediate_steps=False,
             verbose=verbose_level > 1,
-            memory=memory,
         )
 
     def run(self, prompt: str) -> str:
